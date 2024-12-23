e827b5d8b3b77f66314def2f898ff8dfe4b0de93
VIJAY071102 <jsvijayjsv@gmail.com>
2024-12-23 15:31:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero/Hero";
+import Skills from "./components/Skills/Skills";
+import Projects from "./Projects/Projects";
+import ContactMe from "./components/ContactMe/ContactMe";
+import Footer from "./components/Footer/Footer";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Hero />
+        <Skills />
+        <Projects />
+        <ContactMe />
+      </div>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
